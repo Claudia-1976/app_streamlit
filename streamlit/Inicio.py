c2321e4d4085318ea8850d641f158bc4d020814a
Claudia Ilarragorri <cilarragorri@gmail.com>
2025-07-15 00:23:55 -0300

    Update Inicio.py

--- streamlit/Inicio.py
+++ streamlit/Inicio.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import sys
+import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
 from src.utils.constantes import DATASET_INDIVIDUALES, DATASET_HOGARES, NOMBRE_APP
